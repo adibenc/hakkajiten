aff07d6ba13586b22b859b3ad5f834794b22f6d3
adib-enc <adib35785@gmail.com>
2024-07-10 15:14:57 +0700

    add sw dist

---
+++ app/Views/wiki/sc-tech/f3-sw-dist.php
@@ -0,0 +1,258 @@
+<?= $this->extend('layouts/wiki') ?>
+<?= $this->section('title') ?>
+Meta
+<?= $this->endSection() ?>
+<?= $this->section('content') ?>
+<div class="container-fluid">
+	<div role="navigation" class="navbox" aria-labelledby="Software_distribution" style="padding:3px">
+		<table class="nowraplinks mw-collapsible autocollapse navbox-inner mw-made-collapsible"
+			style="border-spacing:0;background:transparent;color:inherit">
+			<tbody>
+				<tr>
+					<th scope="col" class="navbox-title" colspan="2"><button type="button"
+							class="mw-collapsible-toggle mw-collapsible-toggle-default" aria-expanded="true"
+							tabindex="0"><span class="mw-collapsible-text">hide</span></button>
+						<link rel="mw-deduplicated-inline-style" href="mw-data:TemplateStyles:r1129693374">
+						<style data-mw-deduplicate="TemplateStyles:r1063604349">
+							.mw-parser-output .navbar {
+								display: inline;
+								font-size: 88%;
+								font-weight: normal
+							}
+
+							.mw-parser-output .navbar-collapse {
+								float: left;
+								text-align: left
+							}
+
+							.mw-parser-output .navbar-boxtext {
+								word-spacing: 0
+							}
+
+							.mw-parser-output .navbar ul {
+								display: inline-block;
+								white-space: nowrap;
+								line-height: inherit
+							}
+
+							.mw-parser-output .navbar-brackets::before {
+								margin-right: -0.125em;
+								content: "[ "
+							}
+
+							.mw-parser-output .navbar-brackets::after {
+								margin-left: -0.125em;
+								content: " ]"
+							}
+
+							.mw-parser-output .navbar li {
+								word-spacing: -0.125em
+							}
+
+							.mw-parser-output .navbar a>span,
+							.mw-parser-output .navbar a>abbr {
+								text-decoration: inherit
+							}
+
+							.mw-parser-output .navbar-mini abbr {
+								font-variant: small-caps;
+								border-bottom: none;
+								text-decoration: none;
+								cursor: inherit
+							}
+
+							.mw-parser-output .navbar-ct-full {
+								font-size: 114%;
+								margin: 0 7em
+							}
+
+							.mw-parser-output .navbar-ct-mini {
+								font-size: 114%;
+								margin: 0 4em
+							}
+						</style>
+						<div class="navbar plainlinks hlist navbar-mini">
+							<ul>
+								<li class="nv-view"><a href="/wiki/Template:Software_distribution"
+										title="Template:Software distribution"><abbr
+											title="View this template">v</abbr></a></li>
+								<li class="nv-talk"><a href="/wiki/Template_talk:Software_distribution"
+										title="Template talk:Software distribution"><abbr
+											title="Discuss this template">t</abbr></a></li>
+								<li class="nv-edit"><a href="/wiki/Special:EditPage/Template:Software_distribution"
+										title="Special:EditPage/Template:Software distribution"><abbr
+											title="Edit this template">e</abbr></a></li>
+							</ul>
+						</div>
+						<div id="Software_distribution" style="font-size:114%;margin:0 4em"><a
+								href="/wiki/Software_distribution" title="Software distribution">Software
+								distribution</a></div>
+					</th>
+				</tr>
+				<tr>
+					<th scope="row" class="navbox-group" style="width:1%"><a href="/wiki/Software_license"
+							title="Software license">Licenses</a></th>
+					<td class="navbox-list-with-group navbox-list navbox-odd hlist" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/Beerware" title="Beerware">Beerware</a></li>
+								<li><a href="/wiki/Floating_licensing" title="Floating licensing">Floating licensing</a>
+								</li>
+								<li><a href="/wiki/Free_and_open-source_software"
+										title="Free and open-source software">Free and open-source</a>
+									<ul>
+										<li><a href="/wiki/Free_software" title="Free software">Free</a></li>
+										<li><a href="/wiki/Open-source_software" title="Open-source software">Open
+												source</a></li>
+									</ul>
+								</li>
+								<li><a href="/wiki/Freely_redistributable_software"
+										title="Freely redistributable software">Freely redistributable</a></li>
+								<li><a href="/wiki/License-free_software" title="License-free software">License-free</a>
+								</li>
+								<li><a href="/wiki/Proprietary_software" title="Proprietary software">Proprietary</a>
+								</li>
+								<li><a href="/wiki/Public-domain_software" title="Public-domain software">Public
+										domain</a></li>
+								<li><a href="/wiki/Source-available_software"
+										title="Source-available software">Source-available</a></li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row" class="navbox-group" style="width:1%">Compensation models</th>
+					<td class="navbox-list-with-group navbox-list navbox-even hlist" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/Adware" title="Adware">Adware</a></li>
+								<li><a href="/wiki/Commercial_software" title="Commercial software">Commercial
+										software</a>
+									<ul>
+										<li><a href="/wiki/Retail_software" title="Retail software">Retail software</a>
+										</li>
+									</ul>
+								</li>
+								<li><a href="/wiki/Crippleware" title="Crippleware">Crippleware</a></li>
+								<li><a href="/wiki/Crowdfunding" title="Crowdfunding">Crowdfunding</a></li>
+								<li><a href="/wiki/Freemium" title="Freemium">Freemium</a></li>
+								<li><a href="/wiki/Freeware" title="Freeware">Freeware</a></li>
+								<li><a href="/wiki/Pay_what_you_want" title="Pay what you want">Pay what you want</a>
+									<ul>
+										<li><a href="/wiki/Careware" title="Careware">Careware</a></li>
+										<li><a href="/wiki/Donationware" title="Donationware">Donationware</a></li>
+									</ul>
+								</li>
+								<li><a href="/wiki/Open-core_model" title="Open-core model">Open-core model</a></li>
+								<li><a href="/wiki/Postcardware" class="mw-redirect"
+										title="Postcardware">Postcardware</a></li>
+								<li><a href="/wiki/Shareware" title="Shareware">Shareware</a>
+									<ul>
+										<li><a href="/wiki/Nagware" class="mw-redirect" title="Nagware">Nagware</a></li>
+									</ul>
+								</li>
+								<li><a href="/wiki/Trialware" class="mw-redirect" title="Trialware">Trialware</a></li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row" class="navbox-group" style="width:1%">Delivery methods</th>
+					<td class="navbox-list-with-group navbox-list navbox-odd hlist" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/Digital_distribution" title="Digital distribution">Digital
+										distribution</a></li>
+								<li><a href="/wiki/File_sharing" title="File sharing">File sharing</a></li>
+								<li><a class="mw-selflink selflink">On-premises</a></li>
+								<li><a href="/wiki/Pre-installed_software"
+										title="Pre-installed software">Pre-installed</a></li>
+								<li><a href="/wiki/Product_bundling" title="Product bundling">Product bundling</a></li>
+								<li><a href="/wiki/Retail_software" title="Retail software">Retail software</a></li>
+								<li><a href="/wiki/Sneakernet" title="Sneakernet">Sneakernet</a></li>
+								<li><a href="/wiki/Software_as_a_service" title="Software as a service">Software as a
+										service</a></li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row" class="navbox-group" style="width:1%">Deceptive and/or illicit</th>
+					<td class="navbox-list-with-group navbox-list navbox-even hlist" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/Unwanted_software_bundling" class="mw-redirect"
+										title="Unwanted software bundling">Unwanted software bundling</a></li>
+								<li><a href="/wiki/Malware" title="Malware">Malware</a>
+									<ul>
+										<li><a href="/wiki/Spyware" title="Spyware">Spyware</a></li>
+										<li><a href="/wiki/Trojan_horse_(computing)"
+												title="Trojan horse (computing)">Trojan horse</a></li>
+										<li><a href="/wiki/Computer_worm" title="Computer worm">Worm</a></li>
+										<li><a href="/wiki/Ransomware" title="Ransomware">Ransomware</a></li>
+									</ul>
+								</li>
+								<li><a href="/wiki/Scareware" title="Scareware">Scareware</a></li>
+								<li><a href="/wiki/Shovelware" title="Shovelware">Shovelware</a></li>
+								<li><a href="/wiki/Vaporware" title="Vaporware">Vaporware</a>
+									<ul>
+										<li><a href="/wiki/List_of_vaporware" title="List of vaporware">list</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row" class="navbox-group" style="width:1%"><a href="/wiki/Software_release_life_cycle"
+							title="Software release life cycle">Software release life cycle</a></th>
+					<td class="navbox-list-with-group navbox-list navbox-odd hlist" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/Abandonware" title="Abandonware">Abandonware</a></li>
+								<li><a href="/wiki/End-of-life_product" title="End-of-life product">End-of-life</a></li>
+								<li><a href="/wiki/Long-term_support" title="Long-term support">Long-term support</a>
+								</li>
+								<li><a href="/wiki/Software_maintenance" title="Software maintenance">Software
+										maintenance</a></li>
+								<li><a href="/wiki/Software_maintainer" title="Software maintainer">Software
+										maintainer</a></li>
+								<li><a href="/wiki/Software_publisher" title="Software publisher">Software publisher</a>
+								</li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row" class="navbox-group" style="width:1%"><a href="/wiki/Copy_protection"
+							title="Copy protection">Copy protection</a></th>
+					<td class="navbox-list-with-group navbox-list navbox-even hlist" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/Digital_rights_management" title="Digital rights management">Digital
+										rights management</a></li>
+								<li><a href="/wiki/Software_protection_dongle"
+										title="Software protection dongle">Software protection dongle</a></li>
+								<li><a href="/wiki/License_manager" class="mw-redirect" title="License manager">License
+										manager</a></li>
+								<li><a href="/wiki/Product_activation" title="Product activation">Product activation</a>
+								</li>
+								<li><a href="/wiki/Product_key" title="Product key">Product key</a></li>
+								<li><a href="/wiki/Software_copyright" title="Software copyright">Software copyright</a>
+								</li>
+								<li><a href="/wiki/Software_license_server" title="Software license server">Software
+										license server</a></li>
+								<li><a href="/wiki/Software_patent" title="Software patent">Software patent</a></li>
+								<li><a href="/wiki/Torrent_poisoning" title="Torrent poisoning">Torrent poisoning</a>
+								</li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+<?= $this->endSection() ?>
+<?= $this->section('scripts') ?>
+<?= $this->endSection() ?>
