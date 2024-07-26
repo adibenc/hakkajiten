f5d1b8f806de4a2ede96bfa7ec703a9879cdd152
adib-enc <adib35785@gmail.com>
2024-07-26 17:25:14 +0700

    add graphics

---
+++ app/Views/wiki/sc-tech/f3-graphics.php
@@ -0,0 +1,251 @@
+<?= $this->extend('layouts/wiki') ?>
+<?= $this->section('title') ?>
+Meta
+<?= $this->endSection() ?>
+<?= $this->section('content') ?>
+<div class="container-fluid">
+	<div role="navigation" class="navbox" aria-labelledby="Graphics_file_formats" style="padding:3px">
+		<table class="nowraplinks hlist mw-collapsible autocollapse navbox-inner mw-made-collapsible"
+			style="border-spacing:0;background:transparent;color:inherit">
+			<tbody>
+				<tr>
+					<th scope="col" class="navbox-title" colspan="2"><button type="button"
+							class="mw-collapsible-toggle mw-collapsible-toggle-default mw-collapsible-toggle-expanded"
+							aria-expanded="true" tabindex="0"><span class="mw-collapsible-text">hide</span></button>
+						<link rel="mw-deduplicated-inline-style" href="mw-data:TemplateStyles:r1129693374">
+						<link rel="mw-deduplicated-inline-style" href="mw-data:TemplateStyles:r1236085633">
+						<div class="navbar plainlinks hlist navbar-mini">
+							<ul>
+								<li class="nv-view"><a href="/wiki/Template:Graphics_file_formats"
+										title="Template:Graphics file formats"><abbr
+											title="View this template">v</abbr></a></li>
+								<li class="nv-talk"><a href="/wiki/Template_talk:Graphics_file_formats"
+										title="Template talk:Graphics file formats"><abbr
+											title="Discuss this template">t</abbr></a></li>
+								<li class="nv-edit"><a href="/wiki/Special:EditPage/Template:Graphics_file_formats"
+										title="Special:EditPage/Template:Graphics file formats"><abbr
+											title="Edit this template">e</abbr></a></li>
+							</ul>
+						</div>
+						<div id="Graphics_file_formats" style="font-size:114%;margin:0 4em"><a
+								href="/wiki/Image_file_format" title="Image file format">Graphics file formats</a></div>
+					</th>
+				</tr>
+				<tr style="">
+					<th scope="row" class="navbox-group" style="width:1%"><a href="/wiki/Raster_graphics"
+							title="Raster graphics">Raster</a></th>
+					<td class="navbox-list-with-group navbox-list navbox-odd" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/ANI_(file_format)" title="ANI (file format)"> ANI</a></li>
+								<li><a href="/wiki/ANIM" title="ANIM">ANIM</a></li>
+								<li><a href="/wiki/APNG" title="APNG">APNG</a></li>
+								<li><a href="/wiki/ART_image_file_format" title="ART image file format"> ART</a></li>
+								<li><a href="/wiki/AVIF" title="AVIF">AVIF</a></li>
+								<li><a href="/wiki/BMP_file_format" title="BMP file format"> BMP</a></li>
+								<li><a href="/wiki/Better_Portable_Graphics" title="Better Portable Graphics"> BPG</a>
+								</li>
+								<li><a href="/wiki/BSAVE" title="BSAVE"> BSAVE</a></li>
+								<li><a href="/wiki/CALS_Raster_file_format" title="CALS Raster file format"> CAL</a>
+								</li>
+								<li><a href="/wiki/Cineon" title="Cineon"> CIN</a></li>
+								<li><a href="/wiki/Cartesian_Perceptual_Compression"
+										title="Cartesian Perceptual Compression"> CPC</a></li>
+								<li><a href="/wiki/CPT_(file_format)" title="CPT (file format)"> CPT</a></li>
+								<li><a href="/wiki/DirectDraw_Surface" title="DirectDraw Surface"> DDS</a></li>
+								<li><a href="/wiki/Digital_Picture_Exchange" title="Digital Picture Exchange"> DPX</a>
+								</li>
+								<li><a href="/wiki/ECW_(file_format)" title="ECW (file format)"> ECW</a></li>
+								<li><a href="/wiki/OpenEXR" title="OpenEXR"> EXR</a></li>
+								<li><a href="/wiki/FITS" title="FITS">FITS</a></li>
+								<li><a href="/wiki/FLIC_(file_format)" title="FLIC (file format)"> FLIC</a></li>
+								<li><a href="/wiki/Free_Lossless_Image_Format" title="Free Lossless Image Format">
+										FLIF</a></li>
+								<li><a href="/wiki/FlashPix" title="FlashPix"> FPX</a></li>
+								<li><a href="/wiki/GIF" title="GIF">GIF</a></li>
+								<li><a href="/wiki/HDRi_(data_format)" class="mw-redirect" title="HDRi (data format)">
+										HDRi</a></li>
+								<li><a href="/wiki/High_Efficiency_Video_Coding#Main_Still_Picture"
+										title="High Efficiency Video Coding"> HEVC</a></li>
+								<li><a href="/wiki/ICER_(file_format)" title="ICER (file format)">ICER</a></li>
+								<li><a href="/wiki/Apple_Icon_Image_format" title="Apple Icon Image format"> ICNS</a>
+								</li>
+								<li><a href="/wiki/ICO_(file_format)" title="ICO (file format)"> ICO / CUR</a></li>
+								<li><a href="/wiki/Image_Cytometry_Standard" title="Image Cytometry Standard"> ICS</a>
+								</li>
+								<li><a href="/wiki/ILBM" title="ILBM">ILBM</a></li>
+								<li><a href="/wiki/JBIG" title="JBIG">JBIG</a></li>
+								<li><a href="/wiki/JBIG2" title="JBIG2">JBIG2</a></li>
+								<li><a href="/wiki/JPEG_Network_Graphics" title="JPEG Network Graphics"> JNG</a></li>
+								<li><a href="/wiki/JPEG" title="JPEG">JPEG</a></li>
+								<li><a href="/wiki/Lossless_JPEG#JPEG-LS" title="Lossless JPEG"> JPEG-LS</a></li>
+								<li><a href="/wiki/JPEG_2000" title="JPEG 2000">JPEG 2000</a></li>
+								<li><a href="/wiki/JPEG_XL" title="JPEG XL">JPEG XL</a></li>
+								<li><a href="/wiki/JPEG_XR" title="JPEG XR">JPEG XR</a></li>
+								<li><a href="/wiki/JPEG_XS" title="JPEG XS">JPEG XS</a></li>
+								<li><a href="/wiki/JPEG_XT" title="JPEG XT">JPEG XT</a>
+									<ul>
+										<li><a href="/wiki/JPEG-HDR" class="mw-redirect" title="JPEG-HDR">JPEG-HDR</a>
+										</li>
+									</ul>
+								</li>
+								<li><a href="/wiki/Krita" title="Krita"> KRA</a></li>
+								<li><a href="/wiki/Multiple-image_Network_Graphics"
+										title="Multiple-image Network Graphics"> MNG</a></li>
+								<li><a href="/wiki/Magick_Image_File_Format" title="Magick Image File Format"> MIFF</a>
+								</li>
+								<li><a href="/wiki/Nearly_raw_raster_data" class="mw-redirect"
+										title="Nearly raw raster data"> NRRD</a></li>
+								<li><a href="/wiki/OpenRaster" title="OpenRaster"> ORA</a></li>
+								<li><a href="/wiki/Netpbm#PAM_graphics_format" title="Netpbm"> PAM</a></li>
+								<li><a href="/wiki/Netpbm" title="Netpbm"> PBM / PGM / PPM / PNM</a></li>
+								<li><a href="/wiki/PCX" title="PCX">PCX</a></li>
+								<li><a href="/wiki/Progressive_Graphics_File" title="Progressive Graphics File"> PGF</a>
+								</li>
+								<li><a href="/wiki/PICtor_PIC_image_format" title="PICtor PIC image format"> PICtor</a>
+								</li>
+								<li><a href="/wiki/PNG" title="PNG">PNG</a></li>
+								<li><a href="/wiki/Adobe_Photoshop#PSD" title="Adobe Photoshop"> PSD / PSB</a></li>
+								<li><a href="/wiki/PaintShop_Pro" title="PaintShop Pro"> PSP</a></li>
+								<li><a href="/wiki/QOI_(image_format)" title="QOI (image format)"> QOI</a></li>
+								<li><a href="/wiki/QuickTime_VR" title="QuickTime VR"> QTVR</a></li>
+								<li><a href="/wiki/Sun_Raster" title="Sun Raster"> RAS</a></li>
+								<li><a href="/wiki/RGBE_image_format" title="RGBE image format"> RGBE</a>
+									<ul>
+										<li><a href="/wiki/Logluv_TIFF" title="Logluv TIFF">Logluv TIFF</a></li>
+									</ul>
+								</li>
+								<li><a href="/wiki/Silicon_Graphics_Image" title="Silicon Graphics Image"> SGI</a></li>
+								<li><a href="/wiki/Truevision_TGA" title="Truevision TGA"> TGA</a></li>
+								<li><a href="/wiki/Tag_Image_File_Format" class="mw-redirect"
+										title="Tag Image File Format"> TIFF</a>
+									<ul>
+										<li><a href="/wiki/TIFF/EP" title="TIFF/EP">TIFF/EP</a></li>
+										<li><a href="/wiki/Tag_Image_File_Format#TIFF/IT" class="mw-redirect"
+												title="Tag Image File Format"> TIFF/IT</a></li>
+									</ul>
+								</li>
+								<li><a href="/wiki/Ulead_PhotoImpact" title="Ulead PhotoImpact"> UFO / UFP</a></li>
+								<li><a href="/wiki/Wireless_Application_Protocol_Bitmap_Format"
+										title="Wireless Application Protocol Bitmap Format"> WBMP</a></li>
+								<li><a href="/wiki/WebP" title="WebP">WebP</a></li>
+								<li><a href="/wiki/X_BitMap" title="X BitMap"> XBM</a></li>
+								<li><a href="/wiki/XCF_(file_format)" title="XCF (file format)"> XCF</a></li>
+								<li><a href="/wiki/X_PixMap" title="X PixMap"> XPM</a></li>
+								<li><a href="/wiki/Xwd" title="Xwd"> XWD</a></li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr style="">
+					<th scope="row" class="navbox-group" style="width:1%"><a href="/wiki/Raw_image_format"
+							title="Raw image format">Raw</a></th>
+					<td class="navbox-list-with-group navbox-list navbox-even" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/Camera_Image_File_Format" title="Camera Image File Format"> CIFF</a>
+								</li>
+								<li><a href="/wiki/Digital_Negative" title="Digital Negative"> DNG</a></li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr style="">
+					<th scope="row" class="navbox-group" style="width:1%"><a href="/wiki/Vector_graphics"
+							title="Vector graphics">Vector</a></th>
+					<td class="navbox-list-with-group navbox-list navbox-odd" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/Adobe_Illustrator_Artwork" class="mw-redirect"
+										title="Adobe Illustrator Artwork"> AI</a></li>
+								<li><a href="/wiki/CorelDRAW#CDR_file_format" title="CorelDRAW"> CDR</a></li>
+								<li><a href="/wiki/Computer_Graphics_Metafile" title="Computer Graphics Metafile">
+										CGM</a></li>
+								<li><a href="/wiki/AutoCAD_DXF" title="AutoCAD DXF"> DXF</a></li>
+								<li><a href="/wiki/Enhanced_Metafile" class="mw-redirect" title="Enhanced Metafile">
+										EMF</a></li>
+								<li><a href="/wiki/EMF%2B" class="mw-redirect" title="EMF+">EMF+</a></li>
+								<li><a href="/wiki/Gerber_format" title="Gerber format"> Gerber</a></li>
+								<li><a href="/wiki/Haiku_Vector_Icon_Format" class="mw-redirect"
+										title="Haiku Vector Icon Format"> HVIF</a></li>
+								<li><a href="/wiki/IGES" title="IGES">IGES</a></li>
+								<li><a href="/wiki/Precision_Graphics_Markup_Language"
+										title="Precision Graphics Markup Language"> PGML</a></li>
+								<li><a href="/wiki/Scalable_Vector_Graphics" class="mw-redirect"
+										title="Scalable Vector Graphics"> SVG</a></li>
+								<li><a href="/wiki/Vector_Markup_Language" title="Vector Markup Language"> VML</a></li>
+								<li><a href="/wiki/Windows_Metafile" title="Windows Metafile"> WMF</a></li>
+								<li><a href="/wiki/Xar_(graphics)" title="Xar (graphics)"> Xar</a></li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr style="">
+					<th scope="row" class="navbox-group" style="width:1%"><a
+							href="/wiki/Image_file_format#Compound_formats" title="Image file format">Compound</a></th>
+					<td class="navbox-list-with-group navbox-list navbox-even" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/Computable_Document_Format" title="Computable Document Format">
+										CDF</a></li>
+								<li><a href="/wiki/DjVu" title="DjVu">DjVu</a></li>
+								<li><a href="/wiki/Encapsulated_PostScript" title="Encapsulated PostScript"> EPS</a>
+								</li>
+								<li><a href="/wiki/PDF" title="PDF">PDF</a></li>
+								<li><a href="/wiki/PICT" title="PICT">PICT</a></li>
+								<li><a class="mw-selflink selflink"> PS</a></li>
+								<li><a href="/wiki/SWF" title="SWF">SWF</a></li>
+								<li><a href="/wiki/Extensible_Application_Markup_Language"
+										title="Extensible Application Markup Language"> XAML</a></li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr style="">
+					<th scope="row" class="navbox-group" style="width:1%"><a href="/wiki/Metadata"
+							title="Metadata">Metadata</a></th>
+					<td class="navbox-list-with-group navbox-list navbox-odd" style="width:100%;padding:0">
+						<div style="padding:0 0.25em">
+							<ul>
+								<li><a href="/wiki/Exif" title="Exif">Exchangeable image file format (Exif)</a></li>
+								<li><a href="/wiki/International_Press_Telecommunications_Council#Photo_metadata"
+										title="International Press Telecommunications Council">International Press
+										Telecommunications Council §&nbsp;Photo metadata</a></li>
+								<li><a href="/wiki/Extensible_Metadata_Platform"
+										title="Extensible Metadata Platform">Extensible Metadata Platform (XMP)</a></li>
+								<li><a href="/wiki/GIF#Metadata" title="GIF">GIF §&nbsp;Metadata</a></li>
+								<li><a href="/wiki/Steganography" title="Steganography">Steganography</a></li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+				<tr style="">
+					<td class="navbox-abovebelow" colspan="2">
+						<div>
+							<ul>
+								<li><b><span class="noviewer" typeof="mw:File"><span title="Category"><img alt=""
+													src="//upload.wikimedia.org/wikipedia/en/thumb/9/96/Symbol_category_class.svg/16px-Symbol_category_class.svg.png"
+													decoding="async" width="16" height="16" class="mw-file-element"
+													srcset="//upload.wikimedia.org/wikipedia/en/thumb/9/96/Symbol_category_class.svg/23px-Symbol_category_class.svg.png 1.5x, //upload.wikimedia.org/wikipedia/en/thumb/9/96/Symbol_category_class.svg/31px-Symbol_category_class.svg.png 2x"
+													data-file-width="180" data-file-height="185"></span></span> <a
+											href="/wiki/Category:Graphics_file_formats"
+											title="Category:Graphics file formats">Category</a></b></li>
+								<li><b><span class="noviewer" typeof="mw:File"><a href="/wiki/File:Symbol_na_class.svg"
+												class="mw-file-description" title="Non-article page"><img alt=""
+													src="//upload.wikimedia.org/wikipedia/en/thumb/6/6a/Symbol_na_class.svg/16px-Symbol_na_class.svg.png"
+													decoding="async" width="16" height="16" class="mw-file-element"
+													srcset="//upload.wikimedia.org/wikipedia/en/thumb/6/6a/Symbol_na_class.svg/23px-Symbol_na_class.svg.png 1.5x, //upload.wikimedia.org/wikipedia/en/thumb/6/6a/Symbol_na_class.svg/31px-Symbol_na_class.svg.png 2x"
+													data-file-width="180" data-file-height="185"></a></span> <a
+											href="/wiki/Comparison_of_graphics_file_formats"
+											title="Comparison of graphics file formats">Comparison</a></b></li>
+							</ul>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+<?= $this->endSection() ?>
+<?= $this->section('scripts') ?>
+<?= $this->endSection() ?>
